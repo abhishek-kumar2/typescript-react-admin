7c52fe27883639601b34233320388afd58ac7fca
abhishek kumar <abhishek.kumar2@outlook.com>
2022-01-20 22:40:58 +0800

    Users list pagination

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -4,17 +4,34 @@ import axios from 'axios';
 import {User} from "../../models/user";
 
 const Users = () => {
-    const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState([new User()]);
+    const [page, setPage] = useState(1);
+    const [lastPage, setLastPage] = useState(0);
+    const [total, setTotal] = useState(0);
 
     useEffect(() => {
         (
             async () => {
-                const {data} = await axios.get('users');
-                console.log("Users: ", data);
+                const {data} = await axios.get(`users?page=${page}`);
+
                 setUsers(data.data);
+                setLastPage(data.meta.last_page);
+                setTotal(data.meta.total);
             }
         )();
-    }, []);
+    }, [page]);
+
+    const next = () => {
+        if(page < lastPage) {
+            setPage(page + 1);
+        }
+    };
+
+    const previous = () => {
+        if(page > 1) {
+            setPage(page - 1);
+        }
+    };
 
     return (
         <Wrapper>
@@ -34,7 +51,7 @@ const Users = () => {
                         return (
                             <tr key={user.id}>
                                 <td>{user.id}</td>
-                                <td>{user.first_name} {user.last_name}</td>
+                                <td>{user.name}</td>
                                 <td>{user.email}</td>
                                 <td>{user.role.name}</td>
                                 <td></td>
@@ -44,6 +61,18 @@ const Users = () => {
                     </tbody>
                 </table>
             </div>
+            <div className="float-end">Total records: {total}</div>
+            <br/>
+            <nav>
+                <ul className="pagination">
+                    <li className="page-item">
+                        <a className="page-link" onClick={previous}>Previous</a>
+                    </li>
+                    <li className="page-item">
+                        <a className="page-link" onClick={next}>Next</a>
+                    </li>
+                </ul>
+            </nav>
         </Wrapper>
     );
 }
