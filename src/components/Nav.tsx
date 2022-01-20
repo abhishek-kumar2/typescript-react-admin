64cdb121f6d4c06c22e22055c9a51fa683db0819
abhishek kumar <abhishek.kumar2@outlook.com>
2022-01-20 21:28:50 +0800

    NavLink

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -26,13 +26,13 @@ const Nav = () => {
 
     return (
         <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-            <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Company name</a>
+            <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">&#9806; Node-React-Admin</a>
 
             <ul className="my-2 my-md-0 mr-md-3">
-                <Link to="/profile" className="p-2 m-2 text-white text-decoration-none badge rounded-pill bg-secondary">{user.name}</Link>
+                <Link to="/profile" className="p-2 m-2 text-white text-decoration-none">&#128100; {user.name}</Link>
                 <Link to="/login" className="p-2 btn btn-danger"
                     onClick={logout}
-                >Sign out</Link>
+                >&#128274; Logout</Link>
             </ul>
         </nav>
     );
