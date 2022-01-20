690dc7576c0ccaa20561f9289ff77dbdea5fb3e2
abhishek kumar <abhishek.kumar2@outlook.com>
2022-01-20 13:12:00 +0800

    env setup

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Wrapper from "../components/Wrapper";
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <Wrapper>
+                <div>
+                    Dashboard
+                </div>
+            </Wrapper>
+        );
+    }
+}
+
+export default Dashboard;
