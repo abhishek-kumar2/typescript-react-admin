aa7c2b20526b57abdee6ea47d2a871424cbc1b07
abhishek kumar <abhishek.kumar2@outlook.com>
2022-01-24 11:11:47 +0800

    Roles edit

--- src/pages/roles/RoleEdit.tsx
+++ src/pages/roles/RoleEdit.tsx
@@ -1,90 +1,81 @@
 import React, {SyntheticEvent, useEffect, useState} from 'react';
-import {Role} from "../../models/role";
 import { Navigate, useParams } from 'react-router-dom';
 import Wrapper from "../../components/Wrapper";
 import axios from 'axios';
+import {Permission} from "../../models/permission";
 
 const RoleEdit = (props: any) => {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [roleId, setRoleId] = useState('');
-    const [roles, setRoles] = useState([]);
+    const [permissions, setPermissions] = useState([]);
+    const [selected, setSelected] = useState([] as number[]);
+    const [name, setName] = useState('');
     const [redirect, setRedirect] = useState(false);
     const {id} = useParams();
 
     useEffect(() => {
         (
             async () => {
-                const {data} = await axios.get(`users/${id}`);
-                setFirstName(data.first_name);
-                setLastName(data.last_name);
-                setEmail(data.email);
-                setRoleId(data.role.id);
+                const response = await axios.get('permissions');
+                setPermissions(response.data);
 
-                const response = await axios.get('roles');
-                setRoles(response.data);
+                const {data} = await axios.get(`roles/${id}`);
+                setName(data.name);
+                setSelected(data.permissions.map((p: Permission) => p.id));
             }
-        )()
+        )();
     }, []);
+
+    const check = (id: number) => {
+        if(selected.some(s => s === id)) {
+            setSelected(selected.filter(s => s !== id));
+            return;
+        }
+        setSelected([...selected, id]);
+    };
 
     const submit = async (e: SyntheticEvent) => {
         e.preventDefault();
 
         try {
-            await axios.put(`users/${id}`, {
-                first_name: firstName,
-                last_name: lastName,
-                email,
-                role_id: roleId
+            await axios.put(`roles/${id}`, {
+                name,
+                permissions: selected
             });
 
-            setRedirect(true)
+            setRedirect(true);
         } catch (error: any) {
             console.log(`${error.response.status}: ${error.response.statusText}`);
         }
-
     };
 
     if(redirect) {
-        return <Navigate to={'/users'}/>
+        return <Navigate to={'/roles'} />
     }
 
     return (
         <Wrapper>
             <form onSubmit={submit}>
                 <div className="mb-3">
-                    <label htmlFor="firstName" className="form-label">First Name</label>
+                    <label htmlFor="firstName" className="form-label">Name</label>
                     <input type="text" className="form-control" id="firstName"
-                           defaultValue={firstName}
-                           onChange={e => setFirstName(e.target.value)}
+                           defaultValue={name}
+                           onChange={e => setName(e.target.value)}
                     />
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="lastName" className="form-label">Last Name</label>
-                    <input type="text" className="form-control" id="lastName"
-                           defaultValue={lastName}
-                           onChange={e => setLastName(e.target.value)}
-                    />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="emailId" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="emailId"
-                           defaultValue={email}
-                           onChange={e => setEmail(e.target.value)}
-                    />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="role" className="form-label">Role</label>
-                    <select className="form-select" id="role"
-                            value={roleId}
-                            onChange={e => setRoleId(e.target.value)}>
-                        {roles.map((r: Role) => {
-                            return (
-                                <option key={r.id} value={r.id}>{r.name}</option>
-                            )
-                        })}
-                    </select>
+                    {permissions.map((p: Permission) => {
+                        return (
+                            <div className="form-check" key={p.id}>
+                                <input className="form-check-input" type="checkbox"
+                                       value={p.name}
+                                       checked={selected.some(s => s === p.id)}
+                                       onChange={() => check(p.id)}
+                                />
+                                <label className="form-check-label text-capitalize" htmlFor="flexCheckDefault">
+                                    {p.name.replace('_', ' ')}
+                                </label>
+                            </div>
+                        )
+                    })}
                 </div>
 
                 <button className="btn btn-outline-secondary">Save</button>
