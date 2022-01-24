b2e8f1c9d88cf6df18c7e7bd980b89504e8fa9b3
abhishek kumar <abhishek.kumar2@outlook.com>
2022-01-24 19:57:43 +0800

    Oerder's items show/hide and anaimation

--- src/pages/orders/Orders.tsx
+++ src/pages/orders/Orders.tsx
@@ -6,11 +6,22 @@ import {Order} from "../../models/order";
 import Paginator from "../../components/Paginator";
 import {OrderItem} from "../../models/order-item";
 
+const hide = {
+    maxHeight: 0,
+    transition: '500ms ease-in'
+};
+
+const show = {
+    maxHeight: '150px',
+    transition: '500ms ease-out'
+}
+
 const Orders = () => {
     const [orders, setOrders] = useState([]);
     const [page, setPage] = useState(1);
     const [lastPage, setLastPage] = useState(0);
     const [total, setTotal] = useState(0);
+    const [selected, setSelected] = useState(0);
 
     useEffect(() => {
         (
@@ -27,6 +38,10 @@ const Orders = () => {
         )();
     }, [page]);
 
+    const select = (id: number) => {
+        setSelected(selected === id ? 0 : id);
+    };
+
     return (
         <Wrapper>
             <div className="pt-3 pb-2 mb-3 border-bottom">
@@ -35,7 +50,7 @@ const Orders = () => {
             <div className="table-responsive">
                 <table className="table table-striped table-sm">
                     <thead>
-                        <tr>
+                        <tr key="user-head">
                             <th scope="col">#</th>
                             <th scope="col">Name</th>
                             <th scope="col">Email</th>
@@ -47,19 +62,23 @@ const Orders = () => {
                     {orders.map((o: Order) => {
                         return (
                             <>
-                                <tr key={o.id}>
+                                <tr key={`${o.id}-user`}>
                                     <td>{o.id}</td>
                                     <td>{o.name}</td>
                                     <td>{o.email}</td>
                                     <td>{o.total}</td>
-                                    <td></td>
+                                    <td>
+                                        <a className={selected === o.id ? "btn btn-sm btn-primary" : "btn btn-sm btn-outline-primary"}
+                                            onClick={() => select(o.id)}
+                                        >View</a>
+                                    </td>
                                 </tr>
-                                <tr>
+                                <tr key={`${o.id}-order`}>
                                     <td colSpan={5}>
-                                        <div>
+                                        <div className="overflow-hidden" style={selected === o.id ? show : hide}>
                                             <table className="table table-sm">
                                                 <thead>
-                                                <tr>
+                                                <tr key={`${o.id}-order-item`}>
                                                     <th>#</th>
                                                     <th>Product Title</th>
                                                     <th>Quantity</th>
@@ -69,7 +88,7 @@ const Orders = () => {
                                                 <tbody>
                                                 {o.order_items.map((i: OrderItem) => {
                                                     return (
-                                                        <tr key={i.id}>
+                                                        <tr key={`${i.id}-item`}>
                                                             <td>{i.id}</td>
                                                             <td>{i.product_title}</td>
                                                             <td>{i.quantity}</td>
@@ -81,7 +100,6 @@ const Orders = () => {
                                             </table>
                                         </div>
                                     </td>
-
                                 </tr>
                             </>
                         )
