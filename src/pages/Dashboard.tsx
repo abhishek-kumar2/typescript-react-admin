2286a26bb3fd31b521dad17f09f3e4d114349798
abhishek kumar <abhishek.kumar2@outlook.com>
2022-01-24 20:39:13 +0800

    Dashboard sales chart

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,16 +1,52 @@
-import React, {Component} from 'react';
+import React, {useEffect} from 'react';
 import Wrapper from "../components/Wrapper";
+import * as c3 from 'c3';
+import axios from 'axios';
 
-class Dashboard extends Component {
-    render() {
-        return (
-            <Wrapper>
-                <div>
-                    Dashboard
-                </div>
-            </Wrapper>
-        );
-    }
+const Dashboard = () => {
+    useEffect(() => {
+        (
+            async () => {
+                const chart = c3.generate({
+                    bindto: '#chart',
+                    data: {
+                        x: 'x',
+                        columns: [
+                            ['x', 1, 2],
+                            ['Sales', '2021-1-1', '2021-2-2'],
+                        ],
+                        types: {
+                            Sales: 'bar'
+                        }
+                    },
+                    axis: {
+                        x: {
+                            type: 'timeseries',
+                            tick: {
+                                format: '%Y-%m-%d'
+                            }
+                        }
+                    }
+                });
+
+                const {data} = await axios.get('chart');
+
+                chart.load({
+                    columns: [
+                        ['x', ...data.map((r: any) => r.date)],
+                        ['Sales', ...data.map((r: any) => r.sum)]
+                    ]
+                })
+            }
+        )();
+    }, []);
+
+    return (
+        <Wrapper>
+            <h2>Daily Sales</h2>
+            <div id="chart">Empty</div>
+        </Wrapper>
+    );
 }
 
 export default Dashboard;
